ed44962ae44a41f602ae4f6c7bf8286df923bbc4
zackwoo <zachary.woo@163.com>
2014-01-21 22:35:23 +0800

    2014/1/21

--- Young.Web/App_Start/RouteConfig.cs
+++ Young.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,8 @@ namespace Young.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.IgnoreRoute("Static/*");
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
